ba9b553ed00652d2346ff719d612bb8f9db55e6e
thinhtruong2109 <truongquangthinh21092004@gmail.com>
2026-02-12 17:10:01 +0700

    init backend

---
+++ backend/e-ticket-booking-system/src/main/java/com/example/e_ticket_booking_system/ETicketBookingSystemApplication.java
@@ -0,0 +1,13 @@
+package com.example.e_ticket_booking_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ETicketBookingSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ETicketBookingSystemApplication.class, args);
+	}
+
+}
---
+++ backend/e-ticket-booking-system/src/test/java/com/example/e_ticket_booking_system/ETicketBookingSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.e_ticket_booking_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ETicketBookingSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
